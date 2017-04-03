e9743c3f9156263a55a20016eb374616558e8039
Knowledgeable Mammoth <T-K-M@users.noreply.github.com>
2017-04-03 17:22:26 +0300

    Create badgescript.js

---
+++ Scripts/badgescript.js
@@ -0,0 +1,44 @@
+'use strict';
+var badges = "";
+(function () {
+    
+    $(document).ready(function () {
+        var badgehtml = '';
+
+        var htmlcode = '';
+
+        $.when(
+            $.get('file:///D:\Desktop\badges.json', function (data) {
+                badges = data['Badges'];         
+            })).done(
+            function () {
+                var i = 0;
+                for (i = 0; i < badges.length; i++) {
+                    htmlcode += createColumn(i);
+                    }
+                $('#badges').append(htmlcode);
+
+            }
+            );
+
+    });
+
+
+    function createColumn(index) {
+        var tempcode = '';
+        
+		tempcode='<div style="display:inline-block">';
+		
+		tempcode+=badges[index].Name; 
+       
+        var i = 0;
+        for (i = 1; i < badges[index].Assigned.length; i++) {
+            tempcode += badges[index].Assigned[i] + '<br />';
+        }
+       tempcode='</div>';
+       return $tempcode.html();
+    }
+  
+
+})();
+
