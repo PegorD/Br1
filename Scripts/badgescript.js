8b178fd8e5230764d7025e80de9064b00096542b
Knowledgeable Mammoth <T-K-M@users.noreply.github.com>
2017-04-04 08:23:42 +0300

    Update badgescript.js

--- Scripts/badgescript.js
+++ Scripts/badgescript.js
@@ -29,7 +29,7 @@ var badges = "";
         
 		tempcode='<div class="badge">';
 		
-		tempcode+=badges[index].Name+ '<br />'; 
+		tempcode+='<u>'+badges[index].Name+ '</u><br />'; 
        
         var i = 0;
         for (i = 1; i < badges[index].Assigned.length; i++) {
